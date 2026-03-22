04de233512edebf6655ea7f0b7f0634e88a3bed7
Anton Melehov <ax7704@gmail.com>
2026-03-22 19:29:10 +0200

    init commmit

---
+++ Ehu.UiTests/BaseUiTest.cs
@@ -0,0 +1,113 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace Ehu.UiTests;
+
+public class BaseUiTest
+{
+    protected IWebDriver Driver = null!;
+    protected WebDriverWait Wait = null!;
+
+    [SetUp]
+    public void SetUp()
+    {
+        var options = new ChromeOptions();
+        options.AddArgument("--start-maximized");
+
+        Driver = new ChromeDriver(options);
+        Wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(15));
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        try
+        {
+            Driver.Quit();
+            Driver.Dispose();
+        }
+        catch
+        {
+        }
+    }
+
+    protected IWebElement WaitForVisible(By by)
+    {
+        return Wait.Until(driver =>
+        {
+            try
+            {
+                var element = driver.FindElement(by);
+                return element.Displayed ? element : null;
+            }
+            catch
+            {
+                return null;
+            }
+        });
+    }
+
+    protected IWebElement WaitForClickable(By by)
+    {
+        return Wait.Until(driver =>
+        {
+            try
+            {
+                var element = driver.FindElement(by);
+                return (element.Displayed && element.Enabled) ? element : null;
+            }
+            catch
+            {
+                return null;
+            }
+        });
+    }
+
+    protected void AcceptCookiesIfPresent()
+    {
+        string xpath = "//button[contains(., 'I agree')]";
+        
+        try
+        {
+            var buttons = Driver.FindElements(By.XPath(xpath));
+            var button = buttons.FirstOrDefault(b => b.Displayed && b.Enabled);
+
+            if (button != null)
+            {
+                button.Click();
+                Thread.Sleep(500);
+            }
+        }
+        catch
+        {
+        }
+    }
+
+    protected void OpenHomePage()
+    {
+        Driver.Navigate().GoToUrl("https://en.ehuniversity.lt/");
+    }
+
+    protected string GetNormalizedBodyText()
+    {
+        return Driver.FindElement(By.TagName("body"))
+            .Text
+            .Replace('\u00A0', ' ')
+            .Trim();
+    }
+    
+    protected void JsClick(IWebElement element)
+    {
+        ((IJavaScriptExecutor)Driver).ExecuteScript(
+            "arguments[0].scrollIntoView({block: 'center'});", element);
+        ((IJavaScriptExecutor)Driver).ExecuteScript(
+            "arguments[0].click();", element);
+    }
+
+    protected void AssertPageContainsAny(string pageText, params string[] expectedTexts)
+    {
+        Assert.That(expectedTexts.Any(text => pageText.Contains(text)), Is.True);
+    }
+}
---
+++ Ehu.UiTests/EhuTests.cs
@@ -0,0 +1,109 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace Ehu.UiTests;
+
+[TestFixture]
+public class EhuTests : BaseUiTest
+{
+    [Test]
+    public void Test_01_Open_About_Page()
+    {
+        OpenHomePage();
+        AcceptCookiesIfPresent();
+
+        var aboutLink = WaitForClickable(
+            By.XPath("//a[normalize-space()='About']"));
+
+        aboutLink.Click();
+
+        Wait.Until(d => d.Url.Contains("/about/"));
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(Driver.Url, Does.Contain("/about/"));
+            Assert.That(Driver.Title, Does.Contain("About"));
+        });
+
+        var header = WaitForVisible(By.XPath("//h1[contains(normalize-space(), 'About')]"));
+        Assert.That(header.Text, Does.Contain("About"));
+    }
+
+    [Test]
+    public void Test_02_Search_Study_Programs()
+    {
+        OpenHomePage();
+        AcceptCookiesIfPresent();
+
+        var searchInput = Driver.FindElements(
+                By.CssSelector("input[type='search'], input[name='s'], .search-field"))
+            .FirstOrDefault(e => e.Displayed && e.Enabled);
+
+        if (searchInput != null)
+        {
+            searchInput.Clear();
+            searchInput.SendKeys("study programs");
+            searchInput.SendKeys(Keys.Enter);
+        }
+        else
+        {
+            Driver.Navigate().GoToUrl("https://en.ehuniversity.lt/?s=study+programs");
+        }
+
+        Wait.Until(d =>
+            d.Url.Contains("study+programs") ||
+            d.Url.Contains("study%20programs") ||
+            d.PageSource.Contains("Study programs"));
+
+        var pageText = GetNormalizedBodyText().ToLower();
+
+        AssertPageContainsAny(pageText, "study programs", "bachelor", "master");
+    }
+
+    [Test]
+    public void Test_03_Change_Language_To_Lithuanian()
+    {
+        OpenHomePage();
+        AcceptCookiesIfPresent();
+
+        var ltLink = Driver.FindElements(
+                By.XPath("//a[normalize-space()='lt' and contains(@href,'lt.ehuniversity.lt')]"))
+            .FirstOrDefault(e => e.Displayed && e.Enabled);
+
+        if (ltLink != null)
+        {
+            JsClick(ltLink);
+        }
+        else
+        {
+            Driver.Navigate().GoToUrl("https://lt.ehuniversity.lt/");
+        }
+
+        Wait.Until(d => d.Url.Contains("lt.ehuniversity.lt"));
+
+        Assert.That(Driver.Url, Does.Contain("lt.ehuniversity.lt"));
+
+        var bodyText = GetNormalizedBodyText().ToLower();
+
+        AssertPageContainsAny(bodyText, "apie mus", "studijos", "europos humanitarinis universitetas");
+    }
+
+    [Test]
+    public void Test_04_Verify_Contact_Info_Is_Displayed()
+    {
+        Driver.Navigate().GoToUrl("https://en.ehuniversity.lt/contacts/");
+
+        Wait.Until(d => d.Url.Contains("/contacts"));
+
+        var bodyText = GetNormalizedBodyText();
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(bodyText, Does.Contain("consult@ehu.lt"));
+            Assert.That(bodyText, Does.Contain("press@ehu.lt"));
+            Assert.That(bodyText, Does.Contain("office@ehu.lt"));
+            Assert.That(bodyText, Does.Contain("+370 5 263 9650"));
+            Assert.That(bodyText, Does.Contain("Facebook"));
+        });
+    }
+}
