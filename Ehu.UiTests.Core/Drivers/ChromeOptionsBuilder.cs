8ee19485b1f67648c1b9f3ee663bbf786bb0ea35
Anton Melehov <ax7704@gmail.com>
2026-04-06 15:02:59 +0300

    add chrome options builder

---
+++ Ehu.UiTests.Core/Drivers/ChromeOptionsBuilder.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium.Chrome;
+
+namespace Ehu.UiTests.Core.Drivers;
+
+public class ChromeOptionsBuilder
+{
+    private readonly ChromeOptions _options = new();
+
+    public ChromeOptionsBuilder StartMaximized()
+    {
+        _options.AddArgument("--start-maximized");
+        return this;
+    }
+
+    public ChromeOptionsBuilder Headless()
+    {
+        _options.AddArgument("--headless=new");
+        return this;
+    }
+
+    public ChromeOptionsBuilder DisableNotifications()
+    {
+        _options.AddArgument("--disable-notifications");
+        return this;
+    }
+
+    public ChromeOptionsBuilder DisableGpu()
+    {
+        _options.AddArgument("--disable-gpu");
+        return this;
+    }
+
+    public ChromeOptions Build()
+    {
+        return _options;
+    }
+}
