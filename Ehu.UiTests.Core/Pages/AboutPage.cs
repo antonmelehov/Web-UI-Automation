6191284039cb2ce848c9073c2eb9af8e0aa014bc
Anton Melehov <ax7704@gmail.com>
2026-04-06 15:37:07 +0300

    add about page

---
+++ Ehu.UiTests.Core/Pages/AboutPage.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+
+namespace Ehu.UiTests.Core.Pages;
+
+public class AboutPage : BasePage
+{
+    private readonly By _header = By.XPath("//h1[contains(normalize-space(), 'About')]");
+
+    public AboutPage(IWebDriver driver) : base(driver)
+    {
+    }
+
+    public bool IsOpened()
+    {
+        return Driver.Url.Contains("/about/");
+    }
+
+    public string GetHeaderText()
+    {
+        return WaitForVisible(_header).Text;
+    }
+
+    public bool HeaderContains(string expectedText)
+    {
+        return GetHeaderText().Contains(expectedText);
+    }
+}
