2042e78b10aa76e854b8576b07bdbe25691ad031
Anton Melehov <ax7704@gmail.com>
2026-04-06 15:44:05 +0300

    add contacts page

---
+++ Ehu.UiTests.Core/Pages/ContactsPage.cs
@@ -0,0 +1,32 @@
+﻿using Ehu.UiTests.Core.Configuration;
+using OpenQA.Selenium;
+
+namespace Ehu.UiTests.Core.Pages;
+
+public class ContactsPage : BasePage
+{
+    public ContactsPage(IWebDriver driver) : base(driver)
+    {
+    }
+
+    public ContactsPage Open()
+    {
+        OpenUrl(TestSettings.Instance.ContactsPageUrl);
+        return this;
+    }
+
+    public bool IsOpened()
+    {
+        return Driver.Url.Contains("/contacts");
+    }
+
+    public string GetBodyText()
+    {
+        return GetNormalizedBodyText();
+    }
+
+    public bool ContainsText(string expectedText)
+    {
+        return GetBodyText().Contains(expectedText);
+    }
+}
